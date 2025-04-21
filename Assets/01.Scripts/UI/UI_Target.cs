f0c01a15ee383eadd90bfd2e77f969d0a0c56c49
Punksoda <zug3049@gmail.com>
2025-04-21 17:31:13 +0900

    [Feat] Target_UI, Timelinemanager

---
+++ Assets/01.Scripts/UI/UI_Target.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Target : MonoBehaviour
+{
+    public Block block;
+
+    public UI_Target(Block block)
+    {
+        this.block = block;
+    }
+}
