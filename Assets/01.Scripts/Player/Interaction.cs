7e499ad4a9f9d7b5cc05185408e875211818fd4b
Punksoda <zug3049@gmail.com>
2025-04-24 18:01:59 +0900

    [Add] UI_Sequence_Block_Name

--- Assets/01.Scripts/Player/Interaction.cs
+++ Assets/01.Scripts/Player/Interaction.cs
@@ -69,7 +69,6 @@ public class Interaction : MonoBehaviour
                 interactText.gameObject.SetActive(false);
             }
         }
-        
     }
     private void SetText()
     {
--- Assets/01.Scripts/UI/UI_Sequence.cs
+++ Assets/01.Scripts/UI/UI_Sequence.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
-using UnityEngine.Animations;
+using UnityEngine.UI;
 
 public class UI_Sequence : MonoBehaviour
 {
     public Block block;
+    public Text blockName;
 
     public UI_Sequence(Block block)
     {
         this.block = block;
+    }
+
+    public void Initialize(Block block)
+    {
+        this.block = block;
+
+        if (blockName != null && block != null)
+        {
+            blockName.text = block.BlockName;
+        }
     }
 }
