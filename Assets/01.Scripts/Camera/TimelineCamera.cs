f96010256aaf5933961b20c6e39ea04f89349417
CNyaha <llhb0625@gmail.com>
2025-04-24 15:08:43 +0900

    feat_ Timeline순서에 맞게 카메라 전환

--- Assets/01.Scripts/Camera/TimelineCamera.cs
+++ Assets/01.Scripts/Camera/TimelineCamera.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class TimelineCamera : MonoBehaviour
@@ -13,50 +14,55 @@ public class TimelineCamera : MonoBehaviour
         [Tooltip("등록할 카메라")]
         public CinemachineVirtualCamera virtualCamera;
         [Tooltip("카메라 지속시간")]
-        public float duration = 3.0f;
+        public float duration = 4.0f;
     }
 
     [Header("VirtualCamera Setting")]
     public List<CameraEntry> cameraEntries = new List<CameraEntry>();
 
-    private bool hasPlayed = false;
     private Dictionary<int, CameraEntry> cameras;
+    private bool isPlaying = false;
 
     private void Awake()
     {
         cameras = new Dictionary<int, CameraEntry>();
-        foreach (var e in cameraEntries)
+        foreach (var cameraE in cameraEntries)
         {
-            if (e != null && e.virtualCamera)
+            if (cameraE != null && cameraE.virtualCamera)
             {
-                cameras[e.id] = e;
+                cameraE.virtualCamera.gameObject.SetActive(false);
+                cameras[cameraE.id] = cameraE;
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+   public IEnumerator PlayTimelineCam()
     {
-        if (!hasPlayed)
+        if (isPlaying) yield break;
+        isPlaying = true;
+
+
+        foreach (Block block in TimelineManager.Instance.PlacedBlocks)
         {
-            
+            if (cameras.TryGetValue(block.id, out var entry))
+            {
+                // 해당 카메라의 우선순위를 높혀서 활성화
+                entry.virtualCamera.gameObject.SetActive(true);
+                Debug.Log($"{entry.virtualCamera.gameObject.name} 카메라 실행");
+                // FixedTime 만큼 대기
+                yield return new WaitForSeconds(block.FixedTime);
+                // 재생 후 다시 원래 우선순위로 돌려놓기
+                entry.virtualCamera.gameObject.SetActive(false);
+
+
+            }
+            else
+            {
+                Debug.LogWarning($"{block.id} 와 연동된 카메라가 없습니다.");
+            }
         }
+        isPlaying = false;
+
     }
-
-    //private IEnumerator PlaySequnece()
-    //{
-    //    foreach (var e in cameras.Values)
-    //    {
-    //        if (e.virtualCamera != null)
-    //        {
-    //            e.virtualCamera.Priority = 0;
-    //        }
-    //    }
-
-    //    //TODO: 타임라인에 등록된 정보로 카메라 연동
-
-        
-
-    //}
 
 }
