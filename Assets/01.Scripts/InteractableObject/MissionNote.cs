be576cf05a84a13b30b8389cd4be6e665230c59b
THGM1 <jennypsh@naver.com>
2025-04-25 20:41:42 +0900

    [Chore] 코드 정리

--- Assets/01.Scripts/InteractableObject/MissionNote.cs
+++ Assets/01.Scripts/InteractableObject/MissionNote.cs
@@ -39,9 +39,7 @@ public class MissionNote : MonoBehaviour, IInteractable
             Sprite image = null;
             if (!string.IsNullOrEmpty(ImageName))
             {
-                Debug.Log(ImageName);
                 image = ResourceManager.Instance.LoadImage(ImageName);
-                Debug.Log(image);
             }
             questUI.SetQuest(Description, image, DialogText);
             UIManager.Instance.UIActive();
