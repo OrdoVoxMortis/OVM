4d9bc2553da9b8ca855ac44f8a95122b820aa520
Punksoda <zug3049@gmail.com>
2025-04-27 19:27:02 +0900

    [Feat] UI_Sequence (color), QTE_BG image change

--- Assets/01.Scripts/UI/UI_Sequence.cs
+++ Assets/01.Scripts/UI/UI_Sequence.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 public class UI_Sequence : MonoBehaviour
 {
     public Block block;
-    public Color contactBlockColor = Color.red;
     public TextMeshProUGUI blockName;
-
+    public Image blockRange;
+    public Image blockMiddle;
     public void Initialize(Block block)
     {
         this.block = block;
@@ -15,11 +15,34 @@ public class UI_Sequence : MonoBehaviour
         if (blockName != null && block != null)
         {
             blockName.text = block.BlockName;
+            blockRange.color = GetRangeColorById(block.id);
+            blockMiddle.color = GetMiddleColorById(block.id);
         }
+    }
 
-        if(block is ContactBlock)
+    private Color GetRangeColorById(int id)
+    {
+        switch (id) 
         {
-            blockName.color = contactBlockColor;
+            case 4:
+                return new Color32(255,90,70,200); // 접촉
+            case 5:
+                return new Color32(255,250,140,200); // 이벤트
+            default:
+                return new Color32(255,129,0,200); // 기본 색
+        }
+    }
+
+    private Color GetMiddleColorById(int id)
+    {
+        switch (id)
+        {
+            case 4:
+                return Color.red; // 예: Tool
+            case 5:
+                return Color.yellow; // 예: Food
+            default:
+                return new Color32(255,167,6,255); // 기본 색
         }
     }
 }
