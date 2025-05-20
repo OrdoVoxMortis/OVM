8607de2bba174ff4d39e23fc887a2d3c21989cf3
Punksoda <zug3049@gmail.com>
2025-05-20 19:41:58 +0900

    [Feat] Add Loading_Scene
    비동기로 로드 할 수 있는 씬 추가

--- Assets/01.Scripts/Scene/LoadSceneManager.cs
+++ Assets/01.Scripts/Scene/LoadSceneManager.cs
@@ -24,18 +24,8 @@ public class LoadSceneManager : SingleTon<LoadSceneManager>
         asyncOperation.allowSceneActivation = false; //로딩이 완료되는대로 씬을 활성화할것인지
 
         while (!asyncOperation.isDone)
-        { //isDone는 로딩이 완료되었는지 확인하는 변수
-          //time += Time.deltaTime; //시간을 더해줌
-          //print(asyncOperation.progress); //로딩이 얼마나 완료되었는지 0~1의 값으로 보여줌
-
-            //이건 로딩이 너무 빨라서 작성한거라, 무거운 씬 로딩할땐 시간 체크하는 부분은
-            //생략해도 무방하다!
-            //if (time > 5)
-            //{ //3초 기다림(변동가능)
-            //씬 활성화
+        {
             asyncOperation.allowSceneActivation = true;
-            //}
-
             yield return null;
         }
     }
