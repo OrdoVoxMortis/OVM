1cc62bf12732ad5acb04bdd502aad46daf71e0e7
THGM1 <jennypsh@naver.com>
2025-05-20 12:24:34 +0900

    [Feat] 조준 후 상호작용

--- Assets/01.Scripts/InteractableObject/Rifle.cs
+++ Assets/01.Scripts/InteractableObject/Rifle.cs
@@ -1,15 +1,69 @@
 using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rifle : MonoBehaviour, IInteractable
 {
     [SerializeField] private CinemachineVirtualCamera aimCamera;
+    [SerializeField] private float maxDistance = 100f;
+    [SerializeField] private LayerMask interactionLayer;
+    [SerializeField] private TextMeshProUGUI interactText;
+    [SerializeField] private Image crosshair;
+
+    private Camera mainCam;
     private bool isAiming = false;
+    private IInteractable curInteract;
+    private void Start()
+    {
+        mainCam = Camera.main;
+        crosshair.gameObject.SetActive(false);
+        interactText.gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        if (UIManager.Instance.isUIActive)
+        {
+            aimCamera.enabled = false;
+        }
+        else aimCamera.enabled = true;
+        if (isAiming)
+        {
+            Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f);
+            Ray ray = mainCam.ScreenPointToRay(screenCenter);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, interactionLayer))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.white);
+                Debug.Log($"[Ray hit] {hit.collider.name}");
+
+                var interactable = hit.collider.GetComponent<IInteractable>();
+                if (interactable != null)
+                {
+                    curInteract = interactable;
+                    interactText.text = curInteract.GetInteractComponent();
+                    interactText.gameObject.SetActive(true);
+                }
+                else
+                {
+                    interactText.text = string.Empty;
+                    interactText.gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                curInteract = null;
+                interactText.text = string.Empty;
+                interactText.gameObject.SetActive(false);
+                Debug.DrawRay(ray.origin, ray.direction * maxDistance, Color.blue);
+            }
+        }
+    }
     public string GetInteractComponent()
     {
         if (!isAiming) return "E키를 눌러 조준 모드 진입";
+        else if (isAiming && !interactText.gameObject.activeSelf) return "E키를 눌러 조준 모드 해제";
         else return string.Empty;
     }
 
@@ -17,13 +71,18 @@ public class Rifle : MonoBehaviour, IInteractable
     {
         if (aimCamera != null && !isAiming)
         {
+            crosshair.gameObject.SetActive(true);
             aimCamera.Priority = 20;
             isAiming = true;
         }
         else if (isAiming)
         {
+            crosshair.gameObject.SetActive(false);
+            interactText.gameObject.SetActive(false);
             aimCamera.Priority = 0;
             isAiming = false;
+            if(curInteract != null) 
+                curInteract.OnInteract();
         }
     
     }
